06c4d647b1cb15eb9151df8e5614ec475e94aeaa
Momir Milutinović <milutinm462@gmail.com>
2024-06-28 01:10:20 +0200

    feat: Watch movies

--- CloudCinema/src/env/env.ts
+++ CloudCinema/src/env/env.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiHost:'https://ilkmzyipj5.execute-api.us-east-1.amazonaws.com/prod/'
+  apiHost:'https://ilkmzyipj5.execute-api.us-east-1.amazonaws.com/prod/',
+  transcodedMovieBucketUrl: 'https://cloud-cinema-movies-transcoded-bucket-us.s3.amazonaws.com/'
 }
